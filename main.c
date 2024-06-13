6250c886b42c8c98ab388c3e967d2e1b3fed6132
Quanghuy0206 <huy0206huy@gmail.com>
2024-06-14 02:00:52 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,30 @@
 #include "Mylibrary.h"
-
 int main(void) {
-    // Call initialization functions
-    SysClkConf_72MHz();
-    LED_Init();
+    Systick_Init();
     I2C_Init();
     MPU6050_Init();
-    EXTI_Config();
+    delayMs(100);
+    LED_Init();
+    Switch_Init();
     lcd_init();
-    lcd_set_cursor(0, 4);
-    lcd_send_string("bat");
+    set_lcd(0, 4);
+    lcd_send_string("0");
     delayMs(50);
-
     while (1) {
-        // Main program loop
-        if (!system_on) {
-            lcd_clear();
+        if (state == 0) {
+            LED_Active_Reset();
+            Systick_Shutdown();
+        } else {
+            Systick_Init();
+            MPU6050_Read_Accel();
             lcd_set_cursor(0, 4);
-            lcd_send_string("tat");
-            EXTI->IMR &= ~EXTI_IMR_MR0; // disable EXTI0 interrupt
-            enter_sleep_mode();
-            SysClkConf_72MHz(); // reconfigure system clock frequency
-            EXTI->IMR |= EXTI_IMR_MR0; // enable EXTI0 interrupt
+            float Value = AccelValue(Ax,Ay,Az);
+            if (Value > 2.5) {
+               while (1) {
+                   LED_Fall_Detected();
+                   lcd_send_string("1");
+               } 
+            }
         }
     }
 }
