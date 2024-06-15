72c86094cf629f17216c7a7f1f86e2a6485fe7a0
Quanghuy0206 <huy0206huy@gmail.com>
2024-06-15 10:34:11 +0700

    Update Mylibrary.h

--- Mylibrary.h
+++ Mylibrary.h
@@ -1,42 +1,59 @@
 #ifndef MYLIBRARY_H
 #define MYLIBRARY_H
 
-#include "stm32f10x.h"
-#include <stdio.h>
-#include <math.h>
+#include "stm32f1xx_hal.h"  // Include HAL library
 #include <stdint.h>
 
-// Define constants and global variables
+// MPU6050 related functions and definitions
 #define MPU6050_ADDR 0xD0
-#define LCD_ADDR 0x27
 #define SMPLRT_DIV_REG 0x19
-#define GYRO_CONFIG_REG 0x1B 
-#define ACCEL_CONFIG_REG 0x1C 
-#define ACCEL_XOUT_H_REG 0x3B 
-#define TEMP_OUT_H_REG 0x41 
-#define GYRO_XOUT_H_REG 0x43 
-#define PWR_MGMT_1_REG 0x6B 
-#define WHO_AM_I_REG 0X75
+#define GYRO_CONFIG_REG 0x1B
+#define ACCEL_CONFIG_REG 0x1C
+#define ACCEL_XOUT_H_REG 0x3B
+#define TEMP_OUT_H_REG 0x41
+#define GYRO_XOUT_H_REG 0x43
+#define PWR_MGMT_1_REG 0x6B
+#define WHO_AM_I_REG 0x75
 
-extern volatile float Ax, Ay, Az;
-extern volatile float Limit;
-extern volatile uint8_t status;
+extern int16_t Accel_X_RAW;
+extern int16_t Accel_Y_RAW;
+extern int16_t Accel_Z_RAW;
+extern int16_t Gyro_X_RAW;
+extern int16_t Gyro_Y_RAW;
+extern int16_t Gyro_Z_RAW;
+extern float Ax, Ay, Az;
+extern float Gx, Gy, Gz;
 
-// Khai bao ham
-float AccelValue(float ax, float ay, float az);
-void I2C_Init(void);
-void MPU6050_Init(void);
-void LED_Init(void);
-void I2C_Write(uint16_t devAddress, uint8_t regAddress, uint8_t *data, uint16_t size);
-void I2C_Read(uint16_t devAddress, uint8_t regAddress, uint8_t *data, uint16_t size);
+// External I2C handler declaration
+extern I2C_HandleTypeDef hi2c2;
+extern I2C_HandleTypeDef hi2c1;
+
+// LCD related functions
 void lcd_send_cmd(char cmd);
 void lcd_send_data(char data);
-void lcd_send_string(char *str);
-void lcd_init(void);
-void set_lcd(int row, int col);
 void lcd_clear(void);
-void SysClkConf_72MHz(void);
-void EXIT_Config(void);
-void delayMs(uint32_t ms);
+void lcd_put_cur(int row, int col);
+void lcd_init(void);
+void lcd_send_string(char *str);
+
+
+//MPU6050 functions
+void MPU6050_Init(void);
+void MPU6050_Read_Accel(void);
+void MPU6050_Read_Gyro(void);
+float AccelValue(float Ax, float Ay, float Az);
+
+// GPIO Callback function
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
+
+// Fall detection function
+uint8_t fall(void);
+
+// External variables for button states and device state
+void LED_Fall_Detected(void);
+extern volatile int sw1;
+extern volatile int sw2;
+extern uint8_t status;
+extern uint8_t mode_on;
 
 #endif // MYLIBRARY_H
